14ff558cf8ff91fc173f6fe0af37a1ada1b2aef4
Matias Seitour <matias.seitour01@gmail.com>
2023-03-14 17:19:11 -0300

    [MOD] remove import config.js from index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express"
 import cors from "cors"
-import { config } from "./config.js"
 import tasksRoutes from "./routes/tasks.routes.js"
 import authRoutes from "./routes/auth.routes.js"
 import "./utils/auth/index.js"
@@ -15,7 +14,5 @@ app.use(cookieParser());
 app.use("/api/tasks", tasksRoutes);
 app.use("/api/auth", authRoutes);
 
-console.log(config.PORT);
-
-app.listen(process.env.PORT || config.PORT)
+app.listen(process.env.PORT || 3000);
 console.log("server is running")
